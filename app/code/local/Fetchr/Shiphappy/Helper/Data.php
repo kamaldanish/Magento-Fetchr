7aa81016b453de08fde528f0e87cd80ff64e2425
danishkamal <webmaster.danish@gmail.com>
2015-06-23 11:54:49 +0400

    Updating headers

--- app/code/local/Fetchr/Shiphappy/Helper/Data.php
+++ app/code/local/Fetchr/Shiphappy/Helper/Data.php
@@ -14,24 +14,23 @@
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Fetchr Magento Extension to newer
- * versions in the future. If you wish to customize Fetchr Magento Extension (Fetch Shiphappy) for your
+ * versions in the future. If you wish to customize Fetchr Magento Extension (Fetchr Shiphappy) for your
  * needs please refer to http://www.fetchr.us for more information.
  *
  * @author     Danish Kamal
- * @package    Fetch Shiphappy
+ * @package    Fetchr Shiphappy
  * @copyright  Copyright (c) 2015 Fetchr (http://www.fetchr.us)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Fetchr_Shiphappy_Helper_Data extends Mage_Core_Helper_Abstract
 {
     protected $accountType;
-    protected $serviceType;
-    protected $userName;
+  	protected $serviceType;
+	protected $userName;
     protected $password;
     protected $userId;
     protected $connections = array();
-
-    public function init() {
+        public function init() {
         $this->accountType = Mage::getStoreConfig('shiphappy/settings/accounttype');
         $this->serviceType = Mage::getStoreConfig('shiphappy/settings/servicetype');
         $this->userName = Mage::getStoreConfig('shiphappy/settings/username');
--- app/code/local/Fetchr/Shiphappy/Model/Accounttype.php
+++ app/code/local/Fetchr/Shiphappy/Model/Accounttype.php
@@ -14,16 +14,17 @@
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Fetchr Magento Extension to newer
- * versions in the future. If you wish to customize Fetchr Magento Extension (Fetch Shiphappy) for your
+ * versions in the future. If you wish to customize Fetchr Magento Extension (Fetchr Shiphappy) for your
  * needs please refer to http://www.fetchr.us for more information.
  *
  * @author     Danish Kamal
- * @package    Fetch Shiphappy
+ * @package    Fetchr Shiphappy
  * Used in creating options for live|staging config value selection
  * @copyright  Copyright (c) 2015 Fetchr (http://www.fetchr.us)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
- class Fetchr_Shiphappy_Model_Accounttype
+
+class Fetchr_Shiphappy_Model_Accounttype
 {
     public function toOptionArray()
     {
@@ -32,5 +33,4 @@
             array('value'=>'staging', 'label'=>Mage::helper('shiphappy')->__('Staging')),
         );
     }
-
 }
